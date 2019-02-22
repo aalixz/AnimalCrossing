428c2ec90dbc1e6a8f7d67bf68940701e894c3b8
Diogo Moreira <dmoreira.buba@gmail.com>
2019-02-22 16:26:04 +0000

    Bunny ja muda de imagem conforme a direcçao agora so falta o movimento

--- src/org/academiadecodigo/bootcamp/Player.java
+++ src/org/academiadecodigo/bootcamp/Player.java
@@ -15,18 +15,22 @@ public class Player {
     }
 
     public void moveRight(int cellSize) {
+        playerSprite.load("Bunny/BunnyRight.png");
         playerSprite.translate(cellSize, 0);
     }
 
     public void moveLeft(int cellSize) {
+        playerSprite.load("Bunny/BunnyLeft.png");
         playerSprite.translate(-cellSize, 0);
     }
 
     public void moveUp(int cellSize) {
+        playerSprite.load("Bunny/BunnyUp.png");
         playerSprite.translate(0, -cellSize);
     }
 
     public void moveDown(int cellSize) {
+        playerSprite.load("Bunny/BunnyDown.png");
         playerSprite.translate(0, cellSize);
     }
 
