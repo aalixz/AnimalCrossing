b7869f5b963fe669e960b8732a43b45bf0963395
Luis Lopes <lopcarlo@gmail.com>
2019-02-22 23:52:00 +0000

    Sound

--- src/org/academiadecodigo/bootcamp/Test.java
+++ src/org/academiadecodigo/bootcamp/Test.java
@@ -1,9 +1,19 @@
 package org.academiadecodigo.bootcamp;
 
+import java.io.IOException;
+
 public class Test {
     public static void main(String[] args) {
 
         Game slot1 = new Game();
+        try { Sound sound = new Sound();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
         slot1.start();
 
     }
